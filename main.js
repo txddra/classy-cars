d0f712eb76eeef0d56b6fa503476458e651c8cd8
Your Name <you@example.com>
2020-04-09 15:46:56 -0400

    skipped accelerate, got car.wheelCount to work

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 class Vehicle {
-  constructor(name, wheelCount, maxSpeed, speed){
+  constructor(name, wheelCount, maxSpeed){
 this.name = name;
 this.wheelCount = wheelCount;
 this.maxSpeed = maxSpeed;
 this.speed = 0;
+// ✕ increases the speed by a third of maxSpeed (1ms)
+// ✕ doesn't set the speed higher than the maxSpeed
+
   }
+  accelerate(){
+  return ` ${this.speed} + ${this.maxSpeed} / 3 `
+    // return this.speed + this.maxSpeed / 3
+  } 
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(wheelCount){
+    super(wheelCount, 4)
+  }
+  // wheelCount = 4
+  // this.wheelCount = 4
 }
 
 class Ferrari {
